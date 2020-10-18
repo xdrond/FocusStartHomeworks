5fe1ee1bb2aa53995a7cfcb12fbb55066831c7c6
xdrond <xdrond@gmail.com>
2020-10-19 02:09:04 +0700

    Создан проект и описан интерфейс запуска.

---
+++ Car Records/Car Records/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Car Records
+//
+//  Created by xdrond on 16.10.2020.
+//  Copyright © 2020 romanromanov. All rights reserved.
+//
+
+import Foundation
+
+ConsoleInterface().runLoop()
+
