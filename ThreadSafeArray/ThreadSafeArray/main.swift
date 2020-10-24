b1b0ac570097d2f60e2c73b2aee69aafaff5e5eb
xdrond <xdrond@gmail.com>
2020-10-24 14:04:58 +0700

    Изменена структура проекта.
    Новые домашние задания будут выполняться в отдельных проектах, которые теперь размещаются в рамках homeworks.xcworkspace.

---
+++ ThreadSafeArray/ThreadSafeArray/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ThreadSafeArray
+//
+//  Created by xdrond on 24.10.2020.
+//  Copyright © 2020 romanromanov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
