3aac9b490778a22289cd90137a49fd5b2d02d2bb
xdrond <xdrond@gmail.com>
2020-11-01 23:17:07 +0700

    Добавлены ресурсы и расширения для работы с ними.

---
+++ UITabBarCodeLayout/UITabBarCodeLayout/Extensions/Colors.swift
@@ -0,0 +1,22 @@
+//
+//  Colors.swift
+//  UITabBarCodeLayout
+//
+//  Created by xdrond on 01.11.2020.
+//  Copyright © 2020 xdrondrr. All rights reserved.
+//
+
+import UIKit
+
+enum AssetsColor: String {
+	case backgroundColor
+	case fontColor
+}
+
+extension UIColor {
+	static func appColor(_ name: AssetsColor) -> UIColor? {
+		guard let color = UIColor(named: name.rawValue) else { assertionFailure(); return nil }
+		return color
+	}
+}
+
