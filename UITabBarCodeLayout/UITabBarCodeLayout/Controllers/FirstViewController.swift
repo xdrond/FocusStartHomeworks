a6f95d26e091c9d9542d5fd79c833d3d9c2533cc
xdrond <xdrond@gmail.com>
2020-11-02 10:02:14 +0700

    Созданы контроллеры представлений.
    Также добавлены инициализаторы для настройки tabBarItem - чтобы они были прогружены до загрузки view контроллера.

---
+++ UITabBarCodeLayout/UITabBarCodeLayout/Controllers/FirstViewController.swift
@@ -0,0 +1,34 @@
+//
+//  FirstViewController.swift
+//  UITabBarCodeLayout
+//
+//  Created by xdrond on 01.11.2020.
+//  Copyright © 2020 xdrondrr. All rights reserved.
+//
+
+import UIKit
+
+final class FirstViewController: UIViewController {
+
+	// MARK: - Initializers
+	init() {
+		super.init(nibName: nil, bundle: nil)
+		setupTabBarItemAppearances()
+	}
+
+	required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+
+	// MARK: - Lifecycle
+	override func loadView() {
+		self.view = FirstView(frame: .zero)
+	}
+
+	// MARK: - Private Methods
+	private func setupTabBarItemAppearances() {
+		self.tabBarItem.image = UIImage.appImage(.buttonsIcon)
+		self.tabBarItem.title = "Buttons"
+	}
+	
+}
---
+++ UITabBarCodeLayout/UITabBarCodeLayout/Controllers/ThirdViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ThirdViewController.swift
+//  UITabBarCodeLayout
+//
+//  Created by xdrond on 01.11.2020.
+//  Copyright © 2020 xdrondrr. All rights reserved.
+//
+
+import UIKit
+
+final class ThirdViewController: UIViewController {
+
+	// MARK: - Initializers
+	init() {
+		super.init(nibName: nil, bundle: nil)
+		setupTabBarItemAppearances()
+	}
+
+	required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+
+	// MARK: - Lifecycle
+	override func loadView() {
+		self.view = ThirdView(frame: .zero)
+	}
+
+	// MARK: - Private Methods
+	private func setupTabBarItemAppearances() {
+		self.tabBarItem.image = UIImage.appImage(.loginIcon)
+		self.tabBarItem.title = "Login"
+	}
+}
