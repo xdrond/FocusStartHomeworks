4460fedd425f0e168867f0229f1ae55e84120ffe
xdrond <xdrond@gmail.com>
2020-11-02 10:09:07 +0700

    Созданы view

---
+++ UITabBarCodeLayout/UITabBarCodeLayout/Views/FirstView.swift
@@ -0,0 +1,23 @@
+//
+//  FirstView.swift
+//  UITabBarCodeLayout
+//
+//  Created by xdrond on 30.10.2020.
+//  Copyright © 2020 xdrondrr. All rights reserved.
+//
+
+import UIKit
+
+final class FirstView: UIView {
+
+	// MARK: - Initializers
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+	}
+
+	required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+}
+
---
+++ UITabBarCodeLayout/UITabBarCodeLayout/Views/SecondView.swift
@@ -0,0 +1,22 @@
+//
+//  SecondView.swift
+//  UITabBarCodeLayout
+//
+//  Created by xdrond on 30.10.2020.
+//  Copyright © 2020 xdrondrr. All rights reserved.
+//
+
+import UIKit
+
+final class SecondView: UIView {
+
+	// MARK: - Initializers
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+	}
+
+	required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+}
